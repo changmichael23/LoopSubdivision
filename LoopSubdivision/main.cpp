eb9b609df0669cdb6a836f716acac1e48e599074
Michael Chang <changmichael23@gmail.com>
2018-06-06 00:38:12 +0200

    fixed bug for different 3d geometry

--- LoopSubdivision/main.cpp
+++ LoopSubdivision/main.cpp
@@ -59,21 +59,21 @@ float * structToTabColor(std::vector<Point> newPoints, std::vector<Colore> c)
 
 		if (c[j] == Colore(purple))
 		{
-			tabP[i + 6] = RandomFloat(0,1);
-			tabP[i + 7] = RandomFloat(0, 1);
-			tabP[i + 8] = RandomFloat(0, 1);
+			tabP[i + 6] = RandomFloat(0.5,1);
+			tabP[i + 7] = RandomFloat(0.5, 1);
+			tabP[i + 8] = RandomFloat(0.5, 1);
 		}
 		if (c[j] == Colore(red))
 		{
-			tabP[i + 6] = RandomFloat(0, 1);
-			tabP[i + 7] = RandomFloat(0, 1);
-			tabP[i + 8] = RandomFloat(0, 1);
+			tabP[i + 6] = RandomFloat(0.5, 1);
+			tabP[i + 7] = RandomFloat(0.5, 1);
+			tabP[i + 8] = RandomFloat(0.5, 1);
 		}
 		if (c[j] == Colore(blue))
 		{
-			tabP[i + 6] = RandomFloat(0, 1);
-			tabP[i + 7] = RandomFloat(0, 1);
-			tabP[i + 8] = RandomFloat(0, 1);
+			tabP[i + 6] = RandomFloat(0.5, 1);
+			tabP[i + 7] = RandomFloat(0.5, 1);
+			tabP[i + 8] = RandomFloat(0.5, 1);
 		}
 
 		j++;
@@ -92,16 +92,34 @@ bool Initialize()
 	colore[2] = 1.0;
 	colore[3] = 1.0;
 
-	std::vector<Point> centerPoints3D = createRandomPoints(4);
+	std::vector<Point> centerPoints3D = createRandomPoints(50);
+	//std::vector<Point> centerPoints3D;
+	// Tetraede
+	/*centerPoints3D.push_back(Point(250, 0, 250));
+	centerPoints3D.push_back(Point(-250, 0, 250));
+	centerPoints3D.push_back(Point(0, 0, -250));
+	centerPoints3D.push_back(Point(0, 500, 0));*/
+
+
+	// Cube 
+	/*centerPoints3D.push_back(Point(-250, -250, 0));
+	centerPoints3D.push_back(Point(-251, +250, 2));
+	centerPoints3D.push_back(Point(+250, -252, 1));
+	centerPoints3D.push_back(Point(250, 255, 3));
+	centerPoints3D.push_back(Point(-252, -253, -504));
+	centerPoints3D.push_back(Point(-250, +256, -503));
+	centerPoints3D.push_back(Point(+251, -253, -502));
+	centerPoints3D.push_back(Point(254, 252, -501));*/
+
 
 	Graph * tmpGraph = new Graph();
 	EnvInc testEnv = *new EnvInc(tmpGraph,centerPoints3D);
 	testEnv.initializeGraph();
 	testEnv.algo();
-	//tmpFace = testEnv.getGraph()->getFaceList();
+	tmpFace = testEnv.getGraph()->getFaceList();
 	
 	Graph * tmpGraph1 = Graph::duplicateGraph(*testEnv.getGraph());
-	
+	//tmpFace = tmpGraph1->getFaceList();
 	Graph tmpGraph2 = Loop::LoopSubdivision(*tmpGraph1);
 	tmpFace = tmpGraph2.getFaceList();
 	for (int i = 0; i < tmpFace->size(); i++)
@@ -112,6 +130,13 @@ bool Initialize()
 		col.push_back(tmpFace->at(i)->getColor());
 		tmpVectorPoints.push_back(tmpFace->at(i)->getSummitsConnected()->at(2)->getPoint());
 		col.push_back(tmpFace->at(i)->getColor());
+
+		/*tmpVectorPoints.push_back(tmpFace->at(i)->getPoints()[0]);
+		col.push_back(tmpFace->at(i)->getColor());
+		tmpVectorPoints.push_back(tmpFace->at(i)->getPoints()[1]);
+		col.push_back(tmpFace->at(i)->getColor());
+		tmpVectorPoints.push_back(tmpFace->at(i)->getPoints()[2]);
+		col.push_back(tmpFace->at(i)->getColor());*/
 
 	}
 	
